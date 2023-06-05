1197e119b4326d38c48ff1525cfaa1db168b668d
NHOY <NHOY@2210DR000919>
2023-06-05 16:25:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number;
+            int numberStartExample = 0;
+            int stepInExample = 3;
+            int stepInExample2 = 5;
+            int randomNumberMinimum = 0;
+            int randomNumberMaximum = 101;
+            float result;
+            number = random.Next(randomNumberMinimum, randomNumberMaximum);
+
+            Console.WriteLine("Ваше число:" + number);
+
+            result = (number - (number % stepInExample)) + (number - (number % stepInExample2));
+
+            Console.WriteLine("Общая сумма, кратных - " + stepInExample + " и " + stepInExample2 + ", равна - " + result);
+        }
+    }
+}
